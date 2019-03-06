9c19bb74fb19b388255b224844e70c6a1b4696ff
Unknown <wolinskimaria@gmail.com>
2019-03-05 21:38:46 -0600

    Paddle Movement
    I changed up some of the colors and gave the paddle movement.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 myVector = new Vector3(moveHorizontal*25, 0.0f, 0.0f);
+
+        //rb.AddForce(moveHorizontal*50, 0.0f, 0.0f);
+        //rb.AddForce(moveHorizontal, 0.0f, 0.0f, ForceMode.VelocityChange);
+        rb.velocity = myVector;
+    }
+}
